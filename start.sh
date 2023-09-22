ee5cdcecf5eebf6ecd19f34147fc940b5bdb2d97
Teddy Li <tedexe_work.top@foxmail.com>
2023-09-22 23:08:01 +0800

    Revert start.sh

--- start.sh
+++ start.sh
@@ -43,11 +43,10 @@ if ${INPUT_FORCE_WITH_LEASE}; then
     _FORCE_OPTION='--force-with-lease'
 fi
 
-# if ${INPUT_TAGS} ; then
-if true ; then
+if ${INPUT_TAGS} ; then
+# if true ; then
     if test "$INPUT_BRANCH" == 'ALL' ; then
-        # XD_BRANCH_OPTION='--mirror'
-        true
+        XD_BRANCH_OPTION='--mirror'
     else
         _TAGS='--tags'
     fi
